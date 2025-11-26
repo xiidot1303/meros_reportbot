1560e478301603975ad5466a6655cc12454fde5f
xiidot1303 <xii1303@inbox.ru>
2025-11-26 16:16:49 +0500

    change string

--- app/services/string_service.py
+++ app/services/string_service.py
@@ -10,7 +10,7 @@ def order_status_change_string(order: Order, bot_user: Bot_user = None) -> str:
     status_code = order.status
     text = (
         f"""{Strings.new_order[lang] if status_code == 'B#N' else 
-           (Strings.order_status_changed_to[lang] + Order.get_status_label(status_code))}\n\n""" \
+           (Strings.order_status_changed_to[lang] + Order.get_status_label(status_code))}\n""" \
         f"{Strings.order_info[lang]}".format(
             delivery_date = order.delivery_date,
             manager = order.manager,
