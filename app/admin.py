c21f7be703df11599e581f409b9a211343211a19
xiidot1303 <xii1303@inbox.ru>
2025-11-26 14:56:08 +0500

    add order to admin

--- app/admin.py
+++ app/admin.py
@@ -5,3 +5,10 @@ from app.models import *
 class ClientAdmin(admin.ModelAdmin):
     list_display = ('name', 'phone', 'external_id', 'tg_id')
     search_fields = ('name', 'phone', 'external_id', 'tg_id')
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('deal_id', 'client', 'status', 'total_amount', 'delivery_date')
+    search_fields = ('deal_id', 'client__name', 'manager')
+    list_filter = ('status', 'delivery_date')
