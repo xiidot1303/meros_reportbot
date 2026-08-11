aec42ea93be36b673d060c9e7f7bdcd5ef36c8ce
xiidot1303 <xii1303@inbox.ru>
2026-08-11 16:44:34 +0500

    Add warning filter for PTBUserWarning in check_orders command

--- app/management/commands/check_orders.py
+++ app/management/commands/check_orders.py
@@ -1,5 +1,13 @@
 from django.core.management.base import BaseCommand
 from app.scheduled_job.smartup_job import check_orders
+import warnings
+from telegram.warnings import PTBUserWarning
+
+warnings.filterwarnings(
+    "ignore",
+    message=r"If 'per_message=False'.*",
+    category=PTBUserWarning,
+)
 
 class Command(BaseCommand):
     help = 'Update orders list from SmartUp and handle changes'
