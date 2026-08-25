5c4dddd4f2aa0e544b3f9030eeaacfefb1ee0669
xiidot1303 <xii1303@inbox.ru>
2026-08-25 11:00:58 +0500

    Update order archiving logic to exclude already archived orders

--- app/services/order_service.py
+++ app/services/order_service.py
@@ -87,7 +87,7 @@ def handle_orders_change(orders_list: list):
             )
 
     # archived orders
-    for order in Order.objects.exclude(deal_id__in=incoming_ids):
+    for order in Order.objects.exclude(deal_id__in=incoming_ids, status="A"):
         order.status = "A"
         to_update.append(order)
         to_notify_ids.append(order.pk)
