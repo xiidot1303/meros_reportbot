c652897f1ada6a33632c3b2517e4c551df5cb7bb
xiidot1303 <xii1303@inbox.ru>
2026-09-03 10:38:55 +0500

    Refactor uvicorn command: remove file change observer and simplify shutdown handling

--- app/management/commands/run_uvicorn.py
+++ app/management/commands/run_uvicorn.py
@@ -1,10 +1,4 @@
 from django.core.management.base import BaseCommand
-from bot.control.updater import application
-import asyncio
-import os
-import sys
-from watchdog.observers import Observer
-from watchdog.events import FileSystemEventHandler
 
 import signal
 import uvicorn
@@ -32,39 +26,13 @@ async def serve():
         await server.serve()
         await application.stop()
 
-class RestartOnChangeHandler(FileSystemEventHandler):
-    def __init__(self, restart_callback):
-        self.restart_callback = restart_callback
-
-    def on_modified(self, event):
-        if event.is_directory:
-            return
-        if event.src_path.endswith(('.py', '.html', '.css', '.js')):
-            self.restart_callback()
-
-    def on_created(self, event):
-        if event.is_directory:
-            return
-        if event.src_path.endswith(('.py', '.html', '.css', '.js')):
-            self.restart_callback()
-
 class Command(BaseCommand):
     help = 'Start uvicron server'
 
     def handle(self, *args, **options):
-        observer = Observer()
-        event_handler = RestartOnChangeHandler(self.restart)
-        observer.schedule(event_handler, path='.', recursive=True)
-        observer.start()
         try:
             asyncio.run(
                 serve()
             )
         except KeyboardInterrupt:
-            observer.stop()
-        observer.join()
-
-    def restart(self):
-        print("Restarting..\n\n")
-        """Restart the current process."""
-        os.execv(sys.executable, ['python'] + sys.argv)
+            pass
